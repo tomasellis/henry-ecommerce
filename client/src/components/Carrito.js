69fada382c528775c81c014e59fc05eb73b77d16
julyanpatricio <julyan.patricio@gmail.com>
2021-09-01 01:00:01 -0300

    add components base

---
+++ client/src/components/Carrito.js
@@ -0,0 +1,8 @@
+export default function Carrito() {
+
+  return (
+    <div>
+      Carrito
+    </div>   
+  )
+}
---
+++ client/src/components/Search.js
@@ -0,0 +1,15 @@
+
+export default function Search() {
+
+  return (
+    <input
+      id='search-bar'
+      type="text"
+      name="name"
+      autoComplete="off"
+      placeholder='Search'
+    >
+
+    </input>
+  )
+}
