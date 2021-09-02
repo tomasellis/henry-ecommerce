94a51cc6925a97bcbe3f4d8bb1a7d2eeda0b30ef
alexiscjscab <alexiscjscab@gmail.com>
2021-09-02 20:52:27 -0300

    Nav Responsive

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,15 +1,49 @@
+import styled from 'styled-components'
+import SearchIcon from '@material-ui/icons/Search';
+import { IconButton } from '@material-ui/core';
 
 export default function Search() {
 
   return (
-    <input
-      id='search-bar'
-      type="text"
-      name="name"
-      autoComplete="off"
-      placeholder='Search'
-    >
-
-    </input>
+    <InputCtn>
+      <input
+        id='search-bar'
+        type="text"
+        name="name"
+        autoComplete="off"
+        placeholder='Search'
+        clasName='input '
+      >
+      </input>
+      <div>
+        <IconButton>
+          <SearchIcon className='icon'/>
+        </IconButton>
+      </div>
+    </InputCtn>
   )
 }
+
+const InputCtn = styled.div`
+  display: flex;
+  align-items: center;
+
+  input{
+    border: none;
+    outline: 0px;
+    background: rgba(240,240,240,0.5);
+    width: 155px;
+    border-radius: 10px;
+    font-size: 18px;
+    text-align: center;
+  }
+  input::placeholder {
+    text-align: center;
+    color: #000;
+  }
+  
+  .icon{
+    color: #000;
+  }
+
+`
