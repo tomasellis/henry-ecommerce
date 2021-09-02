4354950488ff1aa7bb0a835e8329ad6e140bee53
julyanpatricio <julyan.patricio@gmail.com>
2021-09-02 14:16:28 -0300

    add filters category, genre, color and size

--- server/src/routes/products.ts
+++ server/src/routes/products.ts
@@ -3,24 +3,28 @@ import axios from "axios";
 import {Response, Request, Router, NextFunction} from 'express';
 const router = Router()
 
-// Traeme todos los productos con sus nombres, categorias y generos
-
-const query = `query {
-    producto {
-      nombre
-      categoria
-      genero
-    }
-  }`;
-
 router.get("/", async (req: Request, response: Response, next: NextFunction) => {
+
+  const categoria = req.query.categoria? `categoria: {_eq: ${req.query.categoria}}` : ' '
+  const genero = req.query.genero? `genero: {_eq: ${req.query.genero}}` : ' '
+  const color = req.query.color? `color:{_eq:${req.query.color}}` : ' '
+  const size = req.query.size? `size:{_eq:${req.query.size}}` : ' '
+  
   try {
-    const data = await axios({
+    const {data} = await axios({
       url: "https://henry-pg-api.herokuapp.com/v1/graphql",
       method: "POST",
-      data: { query: query },
+      data: { query: 
+        `query {
+          producto(where: {${categoria},${genero}, opcion_productos: {${color}, ${size}} }) {
+            nombre
+            categoria
+            genero
+          }
+        }`
+      },
     });
-    response.send(data.data.data);
+    response.send(data.data);
   } catch (err) {
     next(err)
   }
