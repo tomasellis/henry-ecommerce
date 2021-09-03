e4c917aaa37099b02fa1532debaf7fac93837f1b
julyanpatricio <julyan.patricio@gmail.com>
2021-09-03 13:53:00 -0300

    add route setAdminUser

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import products from "./products";
 import product from "./product";
-import user from "./user";
+import blockUser from "./blockUser";
+import setAdminUser from './setAdminUser'
 
 const router = Router()
 
 router.use("/products", products);
 router.use("/product", product);
-router.use("/user", user);
+router.use("/blockUser", blockUser);
+router.use("/setAdminUser", setAdminUser);
 
 export default router;
