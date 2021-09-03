d345c6e1e37dba694da0a5fa1bacf38ec440027d
Dylan <dylan011235@gmail.com>
2021-09-03 17:55:51 -0300

    createProduct added/options

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import products from "./products";
 import product from "./product";
 import options from "./options";
+import createProduct from "./createProduct"
 
 const router = Router()
 
 router.use("/products", products);
 router.use("/product", product);
-router.use("/options", options)
+router.use("/options", options);
+router.use("/createProduct", createProduct)
 
 export default router;
