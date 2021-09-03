258a1303fbfb3ed3d0aee5a2b725b49affc4d7ea
ulises2125 <ulisesscardino@gmail.com>
2021-09-03 17:01:02 -0300

    .

--- client/src/actions/index.ts
+++ client/src/actions/index.ts
@@ -2,12 +2,17 @@ import axios from "axios";
 import { Dispatch } from "redux";
 import { ActionTypes } from "./types";
 
+
+const url = "https://henry-pg-api.herokuapp.com/v1/graphql";
+
 export function getArticles(
-    genero,
-    categoria,
+    gender,
+    category,
+    less_than,
+    greater_than,
     color,
     size,
 ) {
-    let string1 = `/products?genero=${genero}&categoria=${categoria}&color=${color}&size=${size}`
+    let string1 = `/products?gender=${gender}&category=${category}&less_than=${less_than}&greater_than=${greater_than}&color=${color}&size=${size}`
 
 }
