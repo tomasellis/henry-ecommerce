ed9618c074e4dfc7471ccd3bf7a85d655f2e1dc7
ulises2125 <ulisesscardino@gmail.com>
2021-09-04 15:07:48 -0300

    actionsf

--- client/src/actions/products/productActions.ts
+++ client/src/actions/products/productActions.ts
@@ -10,7 +10,13 @@ const url = "http://localhost:4000/products?";
 
 export function getArticles(gender,category,less_than,greater_than,color,size){
     return async (dispatch: Dispatch) => {
-        const products = await axios.get(`${url}gender=${gender}&category=${category}&less_tan=${less_than}&greater_than=${greater_than}&color=${color}&size=${size}`)
+        const products = await axios.get(`${url}${
+            gender !== undefined ? `gender=${gender}` : "nada"
+        }&${category !== undefined ? `category=${category}` : "nada"
+        }&${less_than !== undefined ? `less_than=${less_than}` : "nada"
+        }&${greater_than !== undefined ? `greater_than=${greater_than}` : "nada"
+        }&${color !== undefined ? `color=${color}` : "nada"
+        }&${size !== undefined ? `size=${size}` : "nada"}`)
         
     }
 }
