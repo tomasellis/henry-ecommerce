0d0b7be412a9a0d7ea3f6f529275b3d7dcf825b8
Tomás Ellis <tomasitoellis@gmail.com>
2021-09-05 16:46:51 -0300

    fix: minor typos in products.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,22 @@
-import Home from './components/Home/Home'
-import './App.css';
-import Products from './components/products/products'
-import React from 'react';
-import {BrowserRouter ,Route, Switch } from "react-router-dom";
+import Home from "./components/Home/Home";
+import "./App.css";
+import Products from "./components/products/products";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Carrito from "./components/Carrito";
 import Profile from "./components/Profile";
 import NavBar from "./components/NavBar/NavBar";
-import Login from './components/Login/Login';
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path ="/" component={NavBar} />
+      <Route path="/" component={NavBar} />
       <Route exact path="/" component={Home} />
-      <Route exact path = '/clouthing/:gender' component={Products}/>
-      <Route exact path="/login"  component={Login} />
-      <Route exact path="/carrito"  component={Carrito} />
-      <Route exact path="/profile"  component={Profile} />
+      <Route exact path="/clouthing/:gender" component={Products} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/carrito" component={Carrito} />
+      <Route exact path="/profile" component={Profile} />
     </BrowserRouter>
   );
 }
--- client/src/components/products/products.tsx
+++ client/src/components/products/products.tsx
@@ -1,27 +1,26 @@
 import { useParams } from "react-router";
 
 //import css
-import './products.css'
+import "./products.css";
 
 //import components
 import Filter from "./filter/filter";
-import Crads from "./cards/cards";
+import Cards from "./cards/cards";
 
-export default function Products(){
+export default function Products() {
+  type GenderParams = {
+    gender: string;
+  };
+  const { gender } = useParams<GenderParams>();
 
-    type GenderParams = {
-        gender : string
-    };
-    const {gender} = useParams<GenderParams>();
-    
-    return(
-        <div>
-            <h1 className = 'ropa_title_prdouct'>Ropa</h1>
-            <Filter/>
-            <Crads/>
-            {gender === 'men' ? <p>soy hombre</p> : null}
-            {gender === 'woman' ? <p>soy mujer</p> : null}
-            {gender === 'kids' ? <p>soy niño</p> : null}
+  return (
+    <div>
+      <h1 className="ropa_title_prdouct">Ropa</h1>
+      <Filter />
+      <Cards />
+      {gender === "men" ? <p>soy hombre</p> : null}
+      {gender === "woman" ? <p>soy mujer</p> : null}
+      {gender === "kids" ? <p>soy niño</p> : null}
     </div>
-    )   
-};
+  );
+}
