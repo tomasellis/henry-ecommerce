959d68fde52e12cd28a90c349cbb31782c2df30b
Tomás Ellis <tomasitoellis@gmail.com>
2021-09-06 03:13:02 -0300

    feat: working plus, minus and input for quantity

--- client/src/components/Cart/CartProductBox.tsx
+++ client/src/components/Cart/CartProductBox.tsx
@@ -36,6 +36,8 @@ type AddToCartJson = {
   quantity: number;
 };
 
+type FetchInfo = "loading" | "loaded" | "error";
+
 const BASE_URL = process.env.REACT_APP_BASE_BACKEND_URL;
 
 const TESTID = "2c6dc53e-dc41-4cd0-95fe-42451d750711";
@@ -44,6 +46,8 @@ const CartProductBox = (props: CartProductBoxProps) => {
   const [quantity, setQuantity] = useState(
     props.product.productOption.optionQuantity
   );
+
+  const [fetchingInfo, setFetchingInfo] = useState<FetchInfo>("loaded");
 
   const addToCartObj: AddToCartJson = {
     product_option_id: "",
@@ -85,13 +89,42 @@ const CartProductBox = (props: CartProductBoxProps) => {
       </button>
       <input
         className="stockInput"
-        placeholder={"1"}
         value={quantity}
+        disabled={fetchingInfo === "loading" ? true : false}
+        type={"number"}
         onChange={(event) => {
-          console.log(event);
+          event.preventDefault();
+          if (
+            parseInt(event.target.value) >
+            props.product.productOption.optionStock
+          ) {
+            return setQuantity(props.product.productOption.optionStock);
+          }
+          if (
+            event.target.value === "NaN" ||
+            parseInt(event.target.value) <= 0 ||
+            event.target.value === ""
+          ) {
+            return setQuantity(1);
+          }
+          return setQuantity(parseInt(event.target.value));
+        }}
+        onBlur={(event) => {
+          if (fetchingInfo !== "loading") {
+            setFetchingInfo("loading");
+            handleOnChange(quantity, props.product, TESTID);
+            setFetchingInfo("loaded");
+          }
         }}
         min={1}
         max={props.product.productOption.optionStock}
+        onKeyPress={(e) => {
+          if (fetchingInfo !== "loading" && e.key === "Enter") {
+            setFetchingInfo("loading");
+            handleOnChange(quantity, props.product, TESTID);
+            setFetchingInfo("loaded");
+          }
+        }}
       ></input>
       <button
         disabled={
@@ -118,7 +151,7 @@ const CartProductBox = (props: CartProductBoxProps) => {
       >
         x
       </button>
-      <div></div>
+      <div>{fetchingInfo === "loading" ? "LOADING" : ""}</div>
       <div
         style={{
           border: "1px solid",
@@ -133,21 +166,28 @@ const CartProductBox = (props: CartProductBoxProps) => {
 
 export default CartProductBox;
 
+const handleOnChange = async (
+  value: number,
+  product: CartProductData,
+  userId: string
+) => {
+  const data = await axios.post(`${BASE_URL}/addToCart`, {
+    user_id: `${userId}`,
+    product_option_id: `${product.productOption.optionId}`,
+    quantity: value,
+  });
+
+  console.log("handleonchange", data);
+};
+
 const handleOnClick = async (
   sum: boolean,
   product: CartProductData,
   userId: string
 ) => {
-  console.log("llega", {
-    user_id: `${userId}`,
-    product_option_id: `${product.inCartId}`,
-    quantity: sum
-      ? product.productOption.optionQuantity + 1
-      : product.productOption.optionQuantity - 1,
-  });
   const data = await axios.post(`${BASE_URL}/addToCart`, {
     user_id: `${userId}`,
-    product_option_id: `${product.inCartId}`,
+    product_option_id: `${product.productOption.optionId}`,
     quantity: sum
       ? product.productOption.optionQuantity + 1
       : product.productOption.optionQuantity - 1,
@@ -155,3 +195,5 @@ const handleOnClick = async (
 
   console.log("handleonclick", data);
 };
+
+const updateData = async (inCartId: string) => {};
