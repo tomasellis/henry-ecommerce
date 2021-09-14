733ee72c65c85bb1d33bb47a65c6e2fa8a49832e
julyanpatricio <julyan.patricio@gmail.com>
2021-09-14 19:43:36 -0300

    add route for add review

---
+++ server/src/routes/addReview.ts
@@ -0,0 +1,67 @@
+import axios from "axios";
+import { Response, Request, Router, NextFunction } from "express";
+const router = Router();
+require("dotenv").config();
+
+interface Review {
+  user_id: string,
+  id_product_general: string,
+  stars: number,
+  comment:string
+}
+
+
+
+router.post(
+  "/",
+  async (request: Request, response: Response, next: NextFunction) => {
+    const { id_product_general, user_id, stars, comment } = request.body;
+
+    if (
+      id_product_general && user_id && stars && comment 
+    ) {
+
+      const review:Review = {
+        id_product_general,
+        user_id,
+        stars,
+        comment
+    }
+
+      try {
+        const { data } = await axios({
+          url: "https://henry-pg-api.herokuapp.com/v1/graphql",
+          method: "POST",
+          data: {
+            query: `mutation addReview($review: reviews_insert_input!) {
+              insert_reviews_one(object: $review) {
+                comment
+                id_review
+                id_product_general
+                stars
+                user_id
+              }
+            }`, 
+            variables:{review:review}
+          },
+        })
+        if (data.errors) {
+          next(data.errors);
+          return response.send(data.errors);
+        } else {
+          response.json(data.data);
+        }
+      } catch (err) {
+        next(err);
+      }
+    } else {
+      response.json({
+        error:
+          `Missing user_id OR id_product_general OR stars OR comment'`
+      });
+    }
+  }
+);
+
+export default router;
+
