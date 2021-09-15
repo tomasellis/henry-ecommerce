b09846bb7ac7284c8dd8437b4ceafc436fe0d12e
ulises2125 <ulisesscardino@gmail.com>
2021-09-15 14:20:21 -0300

    datosprofile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,13 @@ import "./App.css";
 import Products from "./components/products/products";
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Profile from "./components/Perfil/Profile";
+/* import Profile from "./components/Perfil/Profile"; */
 import NavBar from "./components/NavBar/NavBar";
 import LoggedIn from "./components/Login/LoggedIn";
 import { DetailProductCards } from "./components/Details/DetailProductCards";
 import Add from "./components/Add/Add";
 import PrevCart from "./components/Cart/PrevCart";
+import EditProfile from "./components/Perfil/EditProfile/EditProfile";
 
 function App() {
   return (
@@ -18,10 +19,11 @@ function App() {
       <Route exact path="/created" component={Add} />
       <Route exact path="/loggedIn" component={LoggedIn} />
       <Route exact path="/cart" component={PrevCart} />
-      <Route exact path="/profile" component={Profile} />
+      {/* <Route exact path="/profile" component={Profile} /> */}
       <Route exact path="/clothing/details/:id" component={DetailProductCards}/>
       <Route exact path="/clothing/:gender/:page" component={Products} />
       <Route exact path="/clothing/:id" component={DetailProductCards} />
+      <Route exact path="/editprofile" component={EditProfile} />
     </BrowserRouter>
   );
 }
--- client/src/components/Perfil/EditProfile/EditProfile.tsx
+++ client/src/components/Perfil/EditProfile/EditProfile.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import {useAuth0} from '@auth0/auth0-react'
-import Logout from '../Logout'
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import FormControl from '@material-ui/core/FormControl';
+import './EditProfile.css';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,10 +15,14 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1),
       width: '25ch',
     },
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr",
+    gap:"1em" 
   },
   selectEmpty: {
-    marginTop: theme.spacing(3),
-    width: '25ch'
+    marginTop: theme.spacing(1),
+    width: '25ch',
+    marginLeft: "8px",
   },
   textField: {
     marginLeft: theme.spacing(1),
@@ -26,7 +32,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
-export default function Profile() {
+export default function EditProfile() {
   const {user, isAuthenticated, isLoading} = useAuth0()
   const classes = useStyles();
   if(isLoading) return <div>Loading...</div>
@@ -37,42 +43,44 @@ export default function Profile() {
   };
 
   return (
-    isAuthenticated && <div>
+    <div>
       <div className='div-conteiner'>
                 <h3>Profile</h3>
             <div className='conteiner-perfil'>
                 <h6>-Data</h6>
                 <h6>-Change password</h6>
             </div>
+            <div className="conteiner-datos">
               <form className={classes.root} noValidate autoComplete="off">
-                <div>
                 <TextField
                   id="standard-multiline-flexible"
                   label="Name"
                   multiline
                   maxRows={4}
                   
-                />
+                  />
                 <TextField
                   id="standard-multiline-flexible"
                   label="Last name"
                   multiline
                   maxRows={4}
                   
-                />
-                <InputLabel >Sex</InputLabel>
+                  />
+                <div style={{marginTop:"15px"}}>
+                {/* <InputLabel>Sex</InputLabel> */}
                 <Select
                   labelId="demo-simple-select-helper-label"
-                  placeholder="Select an option"
+                  label="Select an option"
                   id="demo-simple-select-helper"
                   className={classes.selectEmpty}
-                >
+                  >
                   <MenuItem value="">
                     <em>Select an option</em>
                   </MenuItem>
                   <MenuItem value={20}>Male</MenuItem>
                   <MenuItem value={30}>Female</MenuItem>
                 </Select>
+                </div>
                 <TextField
                     id="date"
                     label="Birthday"
@@ -80,30 +88,40 @@ export default function Profile() {
                     defaultValue="00-00-0000"
                     className={classes.textField}
                     InputLabelProps={{
-                      shrink: true,
+                        shrink: true,
                     }}
-                  />
+                    />
                 <TextField
                   id="standard-multiline-flexible"
                   label="DNI"
                   multiline
                   maxRows={4}
                   
+                  />
+                <FormControl className={classes.selectEmpty} disabled>
+                    <InputLabel htmlFor="component-disabled">Email</InputLabel>
+                    <Input id="component-disabled" value="email"/>
+                </FormControl>
+              </form>
+              <form className={classes.root} noValidate autoComplete="off">
+                <TextField
+                    id="filled-secondary"
+                    label="Current Password"
+                    color="secondary"
                 />
                 <TextField
-                  id="standard-multiline-flexible"
-                  label="Email"
-                  multiline
-                  maxRows={4}
-                  
+                    id="filled-secondary"
+                    label="New Password"
+                    color="secondary"
                 />
-                </div>
+                <TextField
+                    id="filled-secondary"
+                    label="Repeat Password"
+                    color="secondary"
+                />
               </form>
+            </div>
         </div>
-      <img src={user.picture} alt={user.name} />
-      <h3>{user.name}</h3>
-      <p>{user.email}</p>
-      <Logout />
     </div>
   )
 }
