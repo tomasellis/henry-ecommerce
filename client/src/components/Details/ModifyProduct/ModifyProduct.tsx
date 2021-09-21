843758ffb9d94526dcac63487b6700f3e4e553cb
Dylan <dylan011235@gmail.com>
2021-09-21 15:54:01 -0300

    modify button created

--- client/src/components/Details/ModifyProduct/ModifyProduct.tsx
+++ client/src/components/Details/ModifyProduct/ModifyProduct.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {  useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from "react-router";
 
@@ -16,7 +16,10 @@ export default function ModifyProduct() {
     dispatch(getProduct(id));
   }, [id, dispatch]);
 
-  console.log(product)
-  return (<div>Hello</div>)
+  return (
+    <div>
+      Modify Product
+    </div>
+  )
 
 }
