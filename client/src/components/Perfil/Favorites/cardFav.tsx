4b43b300ea05d685ff062a42b1d35f11d34ee9cd
Mateoo-pierre <minomateo02@gmail.com>
2021-09-22 09:39:12 -0300

    unicos favs

--- client/src/components/Perfil/Favorites/cardFav.tsx
+++ client/src/components/Perfil/Favorites/cardFav.tsx
@@ -16,12 +16,24 @@ export default function CardFAv({favProductsId}){
            dispatch(getFavProduct(e.product_id)); 
         })
     }, [])
-    const favoritesProducts = useSelector((state : any) => state.favoriteProducts.products);
-    // favoritesProducts.map(e => {
-        //     console.log(e);
-        
-        // })
-        
+    let favoritesProducts = useSelector((state : any) => state.favoriteProducts.products);
+    function removeDuplicates(originalArray, prop) {
+        var newArray = [];
+        var lookupObject  = {};
+   
+        for(var i in originalArray) {
+           lookupObject[originalArray[i][prop]] = originalArray[i];
+        }
+   
+        for(i in lookupObject) {
+            newArray.push(lookupObject[i]);
+        }
+         return newArray;
+    };
+    let arrayUnico = removeDuplicates(favoritesProducts, 'image_url');
+    console.log('soy array unico',arrayUnico);
+    favoritesProducts = arrayUnico;
+    
         
         const IMG = 'https://img.hollisterco.com/is/image/anf/KIC_325-1470-0615-320_prod1?policy=product-medium';
         const IMG_FAV_2 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAzUlEQVRIieWUUQ2DMBRFj4RJQAISKgEJlYCDIWEOQMIcgAPqYBbqgH3QJgVayIPysewm9wfKOeWlAP8WA4x3wTUwueo7BEMg6HPDVQD3VTkFXUTQ5YIXEbhvIQFVwBNomWfcM5+YFNx3DNa3jlHFBEcgaTfRGeE6NaarErsHvyqxQHkEPysRwX1eAoGSwmH5azhqfUYgGdEghSuhYAIeEkGdgDSusXvRrzeV9+phw/KUlO5auKaTCOxq16k0wbqPRGDY7joV/zZGIvidfAFZlMauIVsaSQAAAABJRU5ErkJggg==";
@@ -41,7 +53,7 @@ export default function CardFAv({favProductsId}){
         <div className = 'divqencierratodo'>
          <div className = 'div_fav_products'> 
             {
-                favoritesProducts?.map(e => {
+                arrayUnico?.map(e => {
                     return(
                         <div className = 'cards_container_products_fav'>
                             <div className = 'div_container_card_product_fav'>
