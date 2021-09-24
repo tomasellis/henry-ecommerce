c9e156257989b900df78771d86367cf7f23a3902
Tomás Ellis <tomasitoellis@gmail.com>
2021-09-24 15:33:03 -0300

    feat: merge with dev

--- client/src/components/Perfil/History/DropDownHistory.tsx
+++ client/src/components/Perfil/History/DropDownHistory.tsx
@@ -1,15 +1,8 @@
 import React, { useState } from "react";
-import Box from "@mui/material/Box";
-import Collapse from "@mui/material/Collapse";
 import IconButton from "@mui/material/IconButton";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
+
 import { KeyboardArrowDown, KeyboardArrowUp } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 
